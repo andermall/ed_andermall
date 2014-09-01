0251a59ddf40c2e6313ea6f1c1bc40552587981d
Aluno <Aluno@LAB2PC8>
2014-09-01 20:28:20 -0300

    first commit

---
+++ src/ExercicioAvancado/Jogador.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ExercicioAvancado;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Jogador {
+    
+    private String nome;
+    private int idade;
+    private float altura;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
+    
+}
